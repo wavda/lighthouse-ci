744df4a9a179f8cca3d7aabc8905c7c6e233b266
Wavda Aniva Zabidi <wavda.zabidi@hostinger.com>
2022-10-19 17:12:36 +0700

    update budget to ms

--- .github/lighthouse/desktop-config.js
+++ .github/lighthouse/desktop-config.js
@@ -2,7 +2,6 @@ module.exports = {
     extends: 'lighthouse:default',
     settings: {
       emulatedFormFactor: 'desktop',
-      throttling: { rttMs: 40, throughputKbps: 10240, cpuSlowdownMultiplier: 1 },
       audits: [
         { path: 'metrics/first-contentful-paint', options: { scorePODR: 800, scoreMedian: 1600 } },
         { path: 'metrics/first-meaningful-paint', options: { scorePODR: 800, scoreMedian: 1600 } },
@@ -10,5 +9,5 @@ module.exports = {
         { path: 'metrics/interactive', options: { scorePODR: 2000, scoreMedian: 4500 } },
         { path: 'metrics/first-cpu-idle', options: { scorePODR: 2000, scoreMedian: 4500 } }
       ]
-    }
+    },
   }

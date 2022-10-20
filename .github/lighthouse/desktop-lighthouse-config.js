a16a28ea1f8fbfb6e2c316ec5d3acdf8391e03ac
Wavda Aniva Zabidi <wavda.zabidi@hostinger.com>
2022-10-20 13:45:11 +0700

    improve screenemulation config

--- .github/lighthouse/desktop-lighthouse-config.js
+++ .github/lighthouse/desktop-lighthouse-config.js
@@ -1,8 +1,13 @@
 module.exports = {
     extends: 'lighthouse:default',
     settings: {
+      formFactor: "desktop",
       screenEmulation: {
-        "disabled": true
+        "mobile": false,
+        "width": 1366,
+        "height": 768,
+        "deviceScaleFactor": 2,
+        "emulatedUserAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4695.0 Safari/537.36 Chrome-Lighthouse"
       },
       onlyCategories: ['performance'],
       throttlingMethod: "provided"
